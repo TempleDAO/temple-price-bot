9dc5434216e5b36964344cde0242562a47916323
Dario <medariox@users.noreply.github.com>
2022-07-29 19:36:34 +0200

    Replace OGT price and staked % with Vault TVL and APY

--- temple_sidebar-bot.py
+++ temple_sidebar-bot.py
@@ -29,20 +29,11 @@ def millify(n):
     return '{:.0f}{}'.format(n / 10**(3 * millidx), millnames[millidx])
 
 
-def get_data():
-    query = """query {                                                                  
-      dayProtocolMetrics(first: 2, orderBy: timestamp, orderDirection: desc) {          
-        templePrice                                                                     
-        ogTemplePrice
-        ogTempleSupply
-        ogTempleRatio                                                                   
-        marketCap                                                                       
-        templeCirculatingSupply
-        farmEarnings                                                                  
-      }                                                                                 
-    }"""
+def round2d(n):
+    return round(float(n), 2)
 
-    url = "https://api.thegraph.com/subgraphs/name/templedao/templedao-metrics"
+
+def get_json_data(url, query):
     response = requests.post(url, json={'query': query})
 
     try:
@@ -54,23 +45,77 @@ def get_data():
     if response.status_code != 200 or 'errors' in data:
         raise PriceFetchError(f"Error fetching price {response.text}")
 
+    return data
+
+
+def get_metrics_data():
+    query = """query {
+      dayProtocolMetrics(first: 2, orderBy: timestamp, orderDirection: desc) {
+        templePrice
+        ogTemplePrice
+        ogTempleSupply
+        ogTempleRatio
+        marketCap
+        templeCirculatingSupply
+        farmEarnings
+        totalFarmEarnings
+      }
+    }"""
+
+    url = "https://api.thegraph.com/subgraphs/name/templedao/templedao-metrics"
+    data = get_json_data(url, query)
 
     metrics = data['data']['dayProtocolMetrics'][0]
     prev_metrics = data['data']['dayProtocolMetrics'][1]
 
     data_dict = {
-        'ogTemplePrice': metrics['ogTemplePrice'][:4],
-        'ogTempleSupply': metrics['ogTempleSupply'][:12],
-        'ogTempleRatio': metrics['ogTempleRatio'][:4],
-        'templePrice': metrics['templePrice'][:4],
-        'marketCap': metrics['marketCap'][:12],
-        'templeCirculatingSupply': metrics['templeCirculatingSupply'][:12],
-        'farmRewards_today': metrics['farmEarnings'][:12],
-        'farmRewards_yesterday': prev_metrics['farmEarnings'][:12]
+        'ogTemplePrice': round2d(metrics['ogTemplePrice']),
+        'ogTempleSupply': round2d(metrics['ogTempleSupply']),
+        'ogTempleRatio': round2d(metrics['ogTempleRatio']),
+        'templePrice': round2d(metrics['templePrice']),
+        'marketCap': round2d(metrics['marketCap']),
+        'templeCirculatingSupply': round2d(metrics['templeCirculatingSupply']),
+        'farmRewards_today': round2d(metrics['farmEarnings']),
+        'farmRewards_yesterday': round2d(prev_metrics['farmEarnings'])
     }
 
     return data_dict
 
+
+def get_vault_data(vault_group, days):
+    query = f"""query {{
+      dayProtocolMetrics(first: {days}, orderBy: timestamp, orderDirection: desc) {{
+          totalFarmEarnings
+        }}
+    }}"""
+
+    url = "https://api.thegraph.com/subgraphs/name/templedao/templedao-metrics"
+    data = get_json_data(url, query)
+
+    first_day_earnings = float(data['data']['dayProtocolMetrics'][-1]['totalFarmEarnings'])
+    last_day_earnings = float(data['data']['dayProtocolMetrics'][0]['totalFarmEarnings'])
+    daily_avg_ernings = (last_day_earnings - first_day_earnings) / days;
+
+    query = f"""query {{
+      vaultGroup(id: "{vault_group}") {{
+          tvlUSD
+        }}
+    }}"""
+
+    url = "https://api.thegraph.com/subgraphs/name/templedao/templedao-core"
+    data = get_json_data(url, query)
+
+    tvl = float(data['data']['vaultGroup']['tvlUSD'])
+    apr = (daily_avg_ernings / tvl) * 365 * 100;
+    apy = ((1 + (apr / 100) / 12) ** 12 - 1) * 100;
+
+    data_dict = {
+        'tvl': round2d(tvl),
+        'apr': round2d(apr),
+        'apy': round2d(apy)
+    }
+
+    return data_dict
 
 
 @client.event
@@ -82,24 +127,27 @@ async def on_ready():
 async def _refresh_price():
     logger.info("Refreshing price")
     try:
-        data = get_data()
+        metrics_data = get_metrics_data()
+        vault_data = get_vault_data(vault_group='1m-core', days=14)
     except Exception as err:
         logger.exception('Error refreshing price')
         nickname = 'ERROR'
         activity = 'ERROR'
     else:
-        templeprice = data['templePrice']
-        ogtprice = data['ogTemplePrice']
-        marketcap = millify(float(data['marketCap']))
-        ogTempleSupply = float(data['ogTempleSupply'])
-        ogTempleRatio = float(data['ogTempleRatio'])
-        templeCirculatingSupply = float(data['templeCirculatingSupply'])
-        perc_staked = "{0:.0%}".format((ogTempleSupply * ogTempleRatio / templeCirculatingSupply))
-        dailyFarmEarnings = millify(float(data['farmRewards_today']))
+        templeprice = metrics_data['templePrice']
+        ogtprice = metrics_data['ogTemplePrice']
+        marketcap = millify(metrics_data['marketCap'])
+        ogTempleSupply = metrics_data['ogTempleSupply']
+        ogTempleRatio = metrics_data['ogTempleRatio']
+        templeCirculatingSupply = metrics_data['templeCirculatingSupply']
+        perc_staked = '{0:.0%}'.format((ogTempleSupply * ogTempleRatio / templeCirculatingSupply))
+        dailyFarmEarnings = millify(metrics_data['farmRewards_today'])
+        vault_tvl = millify(vault_data['tvl'])
+        vault_apy = vault_data['apy']
 
-        nickname = f'T ${templeprice} | OG ${ogtprice}'
-        activity = f'Farmed ${dailyFarmEarnings} | S {perc_staked}'
-
+        nickname = f'T ${templeprice} | TVL ${vault_tvl}'
+        activity = f'Farmed ${dailyFarmEarnings} | APY {vault_apy}%'
+    
     logger.info("New stats {nickname} || {activity}", nickname=nickname, activity=activity)
 
     await client.change_presence(activity=discord.Game(name=activity))
